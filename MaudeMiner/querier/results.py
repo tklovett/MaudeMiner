e3d32f2d038a8a0e34e9c8dcb6cb08ef56c9d173
Thomas Lovett <tklovett@gmail.com>
2013-07-16 16:50:33 -0400

    clean up result processing

--- MaudeMiner/querier/results.py
+++ MaudeMiner/querier/results.py
@@ -2,6 +2,8 @@ import os
 from datetime import datetime
 from MaudeMiner.settings import DATA_PATH
 
+
+
 def process_results(result, title="", doWrite=True, query=""):
 	"""write the results of the last SQL query to disk and to screen"""
 
@@ -46,30 +48,42 @@ def process_results(result, title="", doWrite=True, query=""):
 	# print records
 	numRecs = 0
 	for row in result:
-		if (numRecs == 100):
+		# ask user if they want ot stop printing after 100 records
+		if numRecs == 100:
 			i = raw_input("100 records printed. Continue printing records? (y/N) ")
 			if i.lower() not in ["y", "yes"]:
 				doPrint = False
-		# print to screen
+		
 		if doPrint:
-			print "=" * 3 + title + "=" * (77-len(title))
-			for key in row.keys():
-				print key + ":"
-				print "-" * (len(key)+1)
-				print row[key]
-				print ""
-			print "=" * 80
-		# write to txt
+			screen_write_row(row)
+
 		if doWrite:
-			txt.write(template.format(*row) + "\n")
-			csv.write('|'.join(map(str,row)) + "\n")
+			txt_write_row(txt, template, row)
+			csv_write_row(csv, row)
+
 		# count records returned
 		numRecs += 1
 
+	# show number of records returned
+	print "-> %s records returned" % numRecs
 	if doWrite:
 		txt.write(	"-> %s records returned" % numRecs)
-	print "-> %s records returned" % numRecs
 
 	if doWrite:
 		txt.flush()
 		txt.close()
+
+def screen_write_row(row):
+	print "=" * 3 + title + "=" * (77-len(title))
+	for key in row.keys():
+		print key + ":"
+		print "-" * (len(key)+1)
+		print row[key]
+		print ""
+	print "=" * 80
+
+def txt_write_row(txt_file, template, row):
+	txt_file.write(template.format(*row) + "\n")
+
+def csv_write_row(csv_file, row):
+	csv_file.write('|'.join(map(str,row)) + "\n")
